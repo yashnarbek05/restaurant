aef095cd744fedcb7441c75c39d45240cc539136
Abdurahmon <ibrohimovabdurahmon68@gmail.com>
2023-12-29 15:07:57 +0500

    first commit

---
+++ src/main/java/uz/pdp/apprestaurant/AppRestaurantApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.apprestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/apprestaurant/AppRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.apprestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

caf8a3c0ce491954fbce7a5351a6ac6b3cb3a5a8
Tony <yashnarbeksattorov15@gmail.com>
2023-12-29 15:26:27 -0800

    Second commit by Yashnarbek

---
+++ src/main/java/uz/pdp/apprestaurant/model/Food.java
@@ -0,0 +1,16 @@
+package uz.pdp.apprestaurant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class Food {
+    @MongoId
+    private String id;
+}

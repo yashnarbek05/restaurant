0d44578a4baeb3e38f42a86ea51e12de5975b64e
Tony <yashnarbeksattorov15@gmail.com>
2023-12-29 16:10:56 -0800

    Food, Category written

--- src/main/java/uz/pdp/apprestaurant/model/Category.java
+++ src/main/java/uz/pdp/apprestaurant/model/Category.java
@@ -1,23 +1,21 @@
 package uz.pdp.apprestaurant.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
 
 @Document
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
-public class Food {
+public class Category {
     @MongoId
     private String id;
 
     private String name;
-
-    @DBRef(lazy = true)
-    private Category category;
 }
--- src/main/java/uz/pdp/apprestaurant/model/User.java
+++ src/main/java/uz/pdp/apprestaurant/model/User.java
@@ -1,10 +1,8 @@
 package uz.pdp.apprestaurant.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -12,12 +10,7 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
-public class Food {
+public class User {
     @MongoId
     private String id;
-
-    private String name;
-
-    @DBRef(lazy = true)
-    private Category category;
 }
